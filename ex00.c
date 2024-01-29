0e638b8d415daf5393372722eca8de058516d04b
oolivel <158043257+oolivel@users.noreply.github.com>
2024-01-29 15:47:54 +0000

    pcto1g

---
+++ ex00.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    char a = '#';
+    printf ("%c ciaooo\n" , a);
+    return(0);
+}
---
+++ ex01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1;
+    printf("inserisci l'età.\n");
+    scanf("%d", &n1);
+    int n2;
+    printf("inserisci l'età.\n");
+    scanf("%d", &n2);
+    int n3;
+    printf("inserisci l'età.\n");
+    scanf("%d", &n3);
+    int n4;
+    printf("inserisci l'età.\n");
+    scanf("%d", &n4);
+    int n5;
+    printf("inserisci l'età.\n");
+    scanf("%d", &n5);
+    printf("la somma delle età è: %d \n", n1 + n2 + n3 + n4 + n5);
+
+    return (0);
+}
